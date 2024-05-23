9ef879f4e7302a4ad35c7c2d7aab9c0b255af9db
Nikolay Kochev <nick@uni-plovdiv.net>
2024-05-23 16:55:27 +0300

    Added some misc testing code

--- tests/misc_tests.py
+++ tests/misc_tests.py
@@ -4,5 +4,10 @@ sys.path.append("../src/")
 
 from pycheminfo.chemobjects.base import *
 
-a = Atom(symbol = "N")
-print(a)
+a1 = Atom(symbol = "C")
+a2 = Atom(symbol = "N")
+print(a1)
+print(a2)
+
+b = Bond(atom1 = a1)
+print(b)
