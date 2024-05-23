72fb3d095519d2aa4cc719ad8342b799040b0c17
Nikolay Kochev <nick@uni-plovdiv.net>
2024-05-23 16:46:05 +0300

    Added new classes: ChemObjectModel and Bond

--- src/pycheminfo/chemobjects/base.py
+++ src/pycheminfo/chemobjects/base.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
 
-class Atom(BaseModel):
+
+class ChemObjectModel(BaseModel):
+    pass
+
+
+class Atom(ChemObjectModel):
     symbol: str = "C"
     charge: int = 0
     
+
+class Bond(ChemObjectModel):
+    atom1 : Atom = None
+    atom2 : Atom = None
+    order : int = 1
+    pass
+
